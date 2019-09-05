1e4eeb62c6a1ff143801387aa23f9a463d22863a
Joey <joey@bitcoin.com>
2019-09-04 17:49:56 -0700

    Revert "Updating with correct localization strings and implementations"

--- core/models.py
+++ core/models.py
@@ -24,9 +24,9 @@ CURRENCY_RATES = {'USD': 1, 'EUR': 0.88, 'GBP': 0.79, 'AUD': 1.47, 'CAD': 1.32, 
 
 CURRENCY_SIGNS = {'USD': '$', 'EUR': '€', 'GBP': '£', 'AUD': 'AUD $', 'CAD': 'CAD $', 'JPY': '¥'}
 MESSAGES = {}
-THANKS_MESSAGE = _("Thank you!")
+THANKS_MESSAGE = _("Thank you for your service!")
 # Translators: Please provide youtube video about bitcoin in your language
-LOCALIZED_YOUTUBE_URL = _("www.youtube.com/embed/OE3QTbgh-p8")
+LOCALIZED_YOUTUBE_URL = _("www.youtube.com/embed/Gc2en3nHxA4")
 
 
 class Wallet(models.Model):
